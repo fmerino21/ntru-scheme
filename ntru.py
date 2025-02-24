fe9c3197c3afb80f6c7dc6c17eb3630adf414808
fmerino21 <fmerino@unal.edu.co>
2025-02-24 01:53:15 -0500

    initial structure

---
+++ ntru.py
@@ -0,0 +1,137 @@
+import numpy as np
+import sympy as sp
+from sympy.abc import x
+from random import shuffle, seed
+seed(567)
+
+
+
+class NTRU:
+
+    def __init__(self, N, p, q, d):
+        self.N = N
+        self.p = p
+        self.q = q
+        self.d = d
+        self.mod_poly = sp.Poly(x**N - 1, x) 
+
+        if q <= (6*d + 1)*p:
+            raise ValueError("q debe ser mayor que (6d + 1)p para garantizar la correctitud en la desencripción")
+    
+
+    def gen_ternary_polynomial(self, N, d1, d2):
+        """
+        Genera un polinomio ternario con coeficientes en {-1, 0, 1}
+        El polinomio generado tiene d1 coeficientes 1 y d2 coeficientes -1
+        """
+        v = [1]*d1 + [-1]*d2 + [0]*(N-d1-d2)
+        shuffle(v)
+        return sp.Poly(v, x)
+
+
+    def poly_mod(self, f, q):
+        """ 
+        Reduce un polinomio módulo mod_poly en Z_q[x] 
+        """
+        mod_poly = self.mod_poly
+        prod = f % mod_poly
+        return sp.Poly([coef % q for coef in prod.all_coeffs()], x)
+
+
+    def invert_poly_mod_p(self, f, p):
+        """ 
+        Intenta encontrar la inversa de f en Z_p[x] / (x^N - 1) 
+        """
+        N = self.N
+        try:
+            return sp.invert(f, x**N - 1, domain=sp.GF(p))
+        except:
+            raise ValueError(f"f no es invertible en Z_{p}[x]")
+
+
+    def gen_keys(self):
+        """
+        Algoritmo de generacion de llaves
+        Llave publica: h = p * f_q * g
+        Llave privada: f, g, f_p
+        """
+        
+        N = self.N
+        d = self.d
+        p = self.p
+        q = self.q
+        mod_poly = self.mod_poly
+
+        f = self.gen_ternary_polynomial(N, d+1, d)
+        g = self.gen_ternary_polynomial(N, d, d)
+
+        print("---------- Generacion de llaves ---------\n")
+
+        print("f :", f)
+        print("g :", g)
+
+        print("\n---------- Inverso de f en Z_p[x] -------\n")
+
+        f_p = self.invert_poly_mod_p(f, p)            
+        
+        print("f_p :", f_p)
+
+        print("f_p * f :", self.poly_mod(f * f_p, p))
+
+
+        print("\n---------- Inverso de f en Z_q[x] -------\n")
+
+        f_q = self.invert_poly_mod_p(f, q)   
+        
+        print("f_p :", f_q)
+
+        print("f_q * f :", self.poly_mod(f * f_q, q))
+
+        h = self.poly_mod(p * f_q * g, q) 
+
+
+        print("\n---------- Llaves -------\n")
+
+        print("Llave publica: ")
+        print("h :", h)
+
+        return h, f, g, f_p
+
+    def encrypt(self, m, h):
+        """
+        Algoritmo para encriptar un mensaje m
+        """
+        N = self.N
+        d = self.d
+        q = self.q
+
+        r = self.gen_ternary_polynomial(N, d, d)
+        e = self.poly_mod((h * r) + m, q)
+        return e
+
+    def decrypt(self, e, f, f_p_inv):
+        N = self.N
+        q = self.q
+        p = self.p
+
+        a = self.poly_mod(f * e, q)
+        
+        print("\n ------------ Desencripcion ------------------\n")
+
+        print("Polinomio a: ")
+        print(a)
+
+        # Centro y aplico modulo q
+        center_lift = lambda c, q: (c % q - q) if (c % q > q // 2) else c % q
+        a_coeffs = [center_lift(c, q) for c in a.all_coeffs()]
+        lifted_a = sp.Poly(a_coeffs, x)
+        
+        # Centro y aplico modulo p
+        b = self.poly_mod(f_p_inv * lifted_a, p)
+        b_coeffs = [center_lift(c, p) for c in b.all_coeffs()]
+        lifted_b = sp.Poly(b_coeffs, x)
+
+        return lifted_b
+
+
+
---
+++ tests.py
@@ -0,0 +1,25 @@
+from ntru import NTRU
+
+def run_test():
+    # Parametros
+    N = 7
+    p = 3
+    q = 41
+    d = 2
+
+    ntru = NTRU(N, p, q, d)
+
+    # Generar claves
+    h, f, g, f_p = ntru.gen_keys()
+
+    # Mensaje aleatorio
+    m = ntru.gen_ternary_polynomial(N, d, d)
+    print("Mensaje original:", m)
+
+    ciphertext = ntru.encrypt(m, h)
+    print("Texto cifrado:", ciphertext)
+
+    plaintext = ntru.decrypt(ciphertext, f, f_p)
+    print("Texto descifrado:", plaintext)
+
+run_test()
